2b894eaa98ee6c6e0d98ea3b88bcd6d8aae9e2ac
Serdar Vural <serdar.vural@canonical.com>
2023-03-11 15:38:44 +0000

    Initial parser and repo info in toml

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,14 @@
-#[derive(Debug)]
-pub struct Args {}
+use clap::Parser;
 
-impl Args {
-    pub fn parse() -> Self {
-        todo!()
-    }
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+	/// Application name
+	#[arg(long)]
+	app: String,
+	
+	/// Most recent time interval in milliseconds
+	#[arg(short, long, default_value_t = 1000)]
+	interval: u32,
 }
+
